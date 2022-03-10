7b82991e17abbf613538cfbf8d3563210d1bd3be
sisi0808 <sivawqoqpqwu@gmail.com>
2022-03-10 22:57:22 +0900

    210_d

--- abc/210/d.cpp
+++ abc/210/d.cpp
@@ -60,10 +60,57 @@ vector<int> dy = {0, 1, 0, -1};
 const string ALP = "ABCDEFGHIkkKLMNOPQRSTUVWXYZ";
 const string alp = "abcdefghijklmnopqrstuvwxyz";
 
-graph G;
+Graph g, q;
 using mint = modint998244353;
 //using mint = modint1000000007
 
+vector<vector<ll>> rot_90(vector<vector<ll>> IMAGE, int x_size, int y_size){
+
+    vector<vector<ll>> rot(x_size, vector<ll> (y_size));
+    vector<ll> tmp(y_size);
+    int i;
+    int j;
+    for(j = 0; j < x_size; j++){
+        for (i = 0; i < y_size; i++){
+            tmp[i] = IMAGE[y_size-i-1][j];
+        }
+        for(i = 0; i < y_size; i++){
+            rot[j][i] = tmp[i];
+        }
+    }
+    return rot;
+}
+
 int main(void){
     fio();
+    int h,w;
+    ll c;
+    ll ans = INF;
+
+    cin >> h >> w;
+    cin >> c;
+    g.resize(h, vector<ll> (w));
+    rep(i,h) rep(j,w) cin >> g[i][j];
+    q = rot_90(g, w, h);
+
+    //dfs(0, 0);
+
+    rep(i,h) {
+        rep(j,w){
+            if(i != 0) { ans = min(ans, g[i][j]+g[i-1][j] + c); }
+            if(j != 0) { ans = min(ans, g[i][j]+g[i][j-1] + c); }
+            if(i != 0) { g[i][j] = min(g[i][j], g[i-1][j] + c); }
+            if(j != 0) { g[i][j] = min(g[i][j], g[i][j-1] + c); }
+        }
+    }
+    rep(i,w) {
+        rep(j,h){
+            if(i != 0) { ans = min(ans, q[i][j]+q[i-1][j] + c); }
+            if(j != 0) { ans = min(ans, q[i][j]+q[i][j-1] + c); }
+            if(i != 0) { q[i][j] = min(q[i][j], q[i-1][j] + c); }
+            if(j != 0) { q[i][j] = min(q[i][j], q[i][j-1] + c); }
+        }
+    }
+
+    cout << ans << endl;
 }
