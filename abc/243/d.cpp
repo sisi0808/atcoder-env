35bed904783bcd13e6da720665a77fdf53a5ccfb
sisi0808 <sivawqoqpqwu@gmail.com>
2022-03-12 22:47:26 +0900

    243_a-e

---
+++ abc/243/d.cpp
@@ -0,0 +1,107 @@
+#include <bits/stdc++.h>
+/* ACLのライブラリを追加*/
+#include<atcoder/all>
+using namespace atcoder;
+
+#define fio()         \
+    cin.tie(nullptr); \
+    ios::sync_with_stdio(false);
+using namespace std;
+#define rep(i, n) for(int i = 0; i < int(n); ++i)
+#define rrep(i, n) for(int i = n; i >= 0; --i)
+#define repp(i, n, m) for(int i = m; i < int(n); ++i)
+#define fore(i_in, a) for(auto &i_in : a)
+#define ALL(v) (v).begin(), (v).end()
+#define RALL(v) (v).rbegin(), (v).rend()
+#define chmin(a, b) a = min(a, b)
+#define chmax(a, b) a = max(a, b)
+
+// alias g++='g++ -I/mnt/c/Users/Owner/Desktop/ac-library'
+using ll = long long;
+using ld = long double;
+using graph = vector<vector<int>>;
+using Graph = vector<vector<ll>>;
+using P = vector<ll, ll>;
+const int SIZE = 100005;
+const int inf = 1000000000;
+const int modi = 1000000007;
+const long long INF = 10000000000;
+const long long modl = 1000000007LL;
+const long long modll = 998244353LL;
+
+void yes() { cout << "Yes" << endl; }
+void no() { cout << "No" << endl; }
+void yn(bool t) {
+    if(t)
+        yes();
+    else
+        no();
+}
+void Yes() { cout << "YES" << endl; }
+void No() { cout << "NO" << endl; }
+void YN(bool t) {
+    if(t)
+        Yes();
+    else
+        No();
+}
+
+void pri(ll a) { cout << a << endl; }
+void spri(string a) { cout << a << endl; }
+void priV(vector<ll> &vec) {
+    for(size_t i = 0; i < vec.size(); i++) {
+        cout << vec[i] << ":";
+    }
+    cout << endl;
+}
+
+vector<int> dx = {1, 0, -1, 0};
+vector<int> dy = {0, 1, 0, -1};
+const string ALP = "ABCDEFGHIkkKLMNOPQRSTUVWXYZ";
+const string alp = "abcdefghijklmnopqrstuvwxyz";
+
+graph G;
+using mint = modint998244353;
+//using mint = modint1000000007;
+
+int main(void){
+    fio();
+    ll n,x; cin >> n >> x;
+    string s; cin >> s;
+    string t = s;
+    reverse(ALL(t));
+
+
+    ll cou = 0;
+    string a = "";
+    for(auto c: t){
+        if(c == 'U') cou++;
+        else if(cou > 0) cou--;
+        else a += c;
+    }
+
+    string tt = "";
+    while(cou > 0){
+        a += 'U';
+        cou--;
+    }
+
+    reverse(ALL(a));
+
+    //rep(i,n){
+    for(auto c: a){
+        //char c = s[i];
+        if(c == 'U'){
+            x = (x - (x % 2)) / 2;
+        }
+        if(c == 'L'){
+            x *= 2;
+        }
+        if(c == 'R'){
+            x *= 2;
+            x ++;
+        }
+    }
+
+    cout << x << endl;
+}
---
+++ abc/243/e.cpp
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h>
+/* ACLのライブラリを追加*/
+#include<atcoder/all>
+using namespace atcoder;
+
+#define fio()         \
+    cin.tie(nullptr); \
+    ios::sync_with_stdio(false);
+using namespace std;
+#define rep(i, n) for(int i = 0; i < int(n); ++i)
+#define rrep(i, n) for(int i = n; i >= 0; --i)
+#define repp(i, n, m) for(int i = m; i < int(n); ++i)
+#define fore(i_in, a) for(auto &i_in : a)
+#define ALL(v) (v).begin(), (v).end()
+#define RALL(v) (v).rbegin(), (v).rend()
+#define chmin(a, b) a = min(a, b)
+#define chmax(a, b) a = max(a, b)
+
+// alias g++='g++ -I/mnt/c/Users/Owner/Desktop/ac-library'
+using ll = long long;
+using ld = long double;
+using graph = vector<vector<int>>;
+using Graph = vector<vector<ll>>;
+using P = vector<ll, ll>;
+const int SIZE = 100005;
+const int inf = 1000000000;
+const int modi = 1000000007;
+const long long INF = 10000000000;
+const long long modl = 1000000007LL;
+const long long modll = 998244353LL;
+
+void yes() { cout << "Yes" << endl; }
+void no() { cout << "No" << endl; }
+void yn(bool t) {
+    if(t)
+        yes();
+    else
+        no();
+}
+void Yes() { cout << "YES" << endl; }
+void No() { cout << "NO" << endl; }
+void YN(bool t) {
+    if(t)
+        Yes();
+    else
+        No();
+}
+
+void pri(ll a) { cout << a << endl; }
+void spri(string a) { cout << a << endl; }
+void priV(vector<ll> &vec) {
+    for(size_t i = 0; i < vec.size(); i++) {
+        cout << vec[i] << ":";
+    }
+    cout << endl;
+}
+
+vector<int> dx = {1, 0, -1, 0};
+vector<int> dy = {0, 1, 0, -1};
+const string ALP = "ABCDEFGHIkkKLMNOPQRSTUVWXYZ";
+const string alp = "abcdefghijklmnopqrstuvwxyz";
+
+graph G;
+using mint = modint998244353;
+//using mint = modint1000000007;
+
+struct edge { ll u, v; ll cost;};
+
+bool comp(const edge& e1, const edge& e2){
+    return e1.cost < e2.cost;
+}
+
+edge es[1000000];
+int n,m;
+
+// 最小全域木のコストを求める
+ll kruskal(){
+    sort(es, es + m, comp);
+    dsu d(n);
+    ll res = 0;
+    rep(i,m){
+        edge e = es[i];
+        if(!d.same(e.u, e.v)){
+            d.merge(e.u, e.v);
+            res ++;
+        }
+    }
+    return res;
+}
+
+
+int main(void){
+    fio();
+    cin >> n >> m;
+    ll all_cost = 0;
+    rep(i,m){
+        cin >> es[i].u >> es[i].v >> es[i].cost;
+        es[i].u --;
+        es[i].v --;
+    }
+    cout << m - kruskal() << endl;
+}
