e159858a2aa3a22338d88d2fefa7b283baee0117
sisi0808 <sivawqoqpqwu@gmail.com>
2022-03-18 09:59:51 +0900

    203_a-c

---
+++ abc/203/c.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+/* ACLのライブラリを追加*/
+#include<atcoder/all>
+using namespace atcoder;
+
+#define fio()         \
+    cin.tie(nullptr); \
+    ios::sync_with_stdio(false);
+using namespace std;
+#define rep(i, n) for(int i = 0; i < int(n); ++i)
+#define rrep(i, n) for(int i = n; i >= 0; --i)
+#define repp(i, n, m) for(int i = m; i < int(n); ++i)
+#define fore(i_in, a) for(auto &i_in : a)
+#define ALL(v) (v).begin(), (v).end()
+#define RALL(v) (v).rbegin(), (v).rend()
+#define chmin(a, b) a = min(a, b)
+#define chmax(a, b) a = max(a, b)
+
+// alias g++='g++ -I/mnt/c/Users/Owner/Desktop/ac-library'
+using ll = long long;
+using ld = long double;
+using graph = vector<vector<int>>;
+using Graph = vector<vector<ll>>;
+using P = vector<ll, ll>;
+const int SIZE = 100005;
+const int inf = 1000000000;
+const int modi = 1000000007;
+const long long INF = 10000000000;
+const long long modl = 1000000007LL;
+const long long modll = 998244353LL;
+
+void yes() { cout << "Yes" << endl; }
+void no() { cout << "No" << endl; }
+void yn(bool t) {
+    if(t)
+        yes();
+    else
+        no();
+}
+void Yes() { cout << "YES" << endl; }
+void No() { cout << "NO" << endl; }
+void YN(bool t) {
+    if(t)
+        Yes();
+    else
+        No();
+}
+
+void pri(ll a) { cout << a << endl; }
+void spri(string a) { cout << a << endl; }
+void priV(vector<ll> &vec) {
+    for(size_t i = 0; i < vec.size(); i++) {
+        cout << vec[i] << ":";
+    }
+    cout << endl;
+}
+
+vector<int> dx = {1, 0, -1, 0};
+vector<int> dy = {0, 1, 0, -1};
+const string ALP = "ABCDEFGHIkkKLMNOPQRSTUVWXYZ";
+const string alp = "abcdefghijklmnopqrstuvwxyz";
+
+Graph g;
+using mint = modint998244353;
+//using mint = modint1000000007;
+
+int main(void){
+    fio();
+    ll n,k; cin >> n >> k;
+    vector<pair<ll, ll>> a;
+    vector<pair<ll, ll>> b;
+
+    rep(i,n) {
+        ll aa,bb; cin >> aa >> bb;
+        a.push_back({aa, bb});
+    }
+
+    sort(ALL(a));
+
+    b.push_back({0, k});
+    rep(i,n){
+        ll aa,ab;
+        ll ba,bb;
+        tie(aa, ab) = a[i];
+        tie(ba, bb) = b[i];
+        b.push_back({aa, ab+bb});
+    }
+
+    repp(i,n+1,1){
+        //cout << b[i].first << " " <<  b[i].second << endl;
+        if(b[i].first > b[i-1].second){
+            cout << b[i-1].second << endl;
+            return 0;
+        }
+    }
+
+    cout << b[n].second << endl;
+}
