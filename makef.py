f45d9a71e1e54027500aff36002648e489843edc
sisi0808 <sivawqoqpqwu@gmail.com>
2022-04-08 09:09:39 +0900

    Update makef.py

--- makef.py
+++ makef.py
@@ -5,11 +5,15 @@ import sys
 # 指定しなければフォルダ作成
 if __name__ == '__main__':
 
-    # コンテスト名(abc210, typical90, dp)
-    con_name = input('コンテスト名を入力(abc210など)：')
-
-    mcf = Mcf.MakeContestFolder(con_name)
     if len(sys.argv) >= 2 and sys.argv[1] == 'del':
+        # コンテスト名(abc210, typical90, dp)
+        con_name = input('削除するコンテスト名を入力(abc210など)：')
+
+        mcf = Mcf.MakeContestFolder(con_name)
         mcf.del_contest_dir()
     else:
+        # コンテスト名(abc210, typical90, dp)
+        con_name = input('コンテスト名を入力(abc210など)：')
+
+        mcf = Mcf.MakeContestFolder(con_name)
         mcf.make_contest_dir()
