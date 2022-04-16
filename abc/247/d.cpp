8cebe394b9555a0411c71c53d74a95a6a447af45
sisi0808 <sivawqoqpqwu@gmail.com>
2022-04-16 11:45:41 +0900

    abc247_a-d

---
+++ abc/247/d.cpp
@@ -0,0 +1,106 @@
+#include <bits/stdc++.h>
+/* ACLのライブラリを追加*/
+#include <atcoder/all>
+using namespace atcoder;
+
+#define fio()         \
+    cin.tie(nullptr); \
+    ios::sync_with_stdio(false);
+using namespace std;
+#define rep(i, n) for(int i = 0; i < int(n); ++i)
+#define rrep(i, n) for(int i = n; i >= 0; --i)
+#define repp(i, n, m) for(int i = m; i < int(n); ++i)
+#define fore(i_in, a) for(auto &i_in : a)
+#define ALL(v) (v).begin(), (v).end()
+#define RALL(v) (v).rbegin(), (v).rend()
+#define chmin(a, b) a = min((ll)a, (ll)b)
+#define chmax(a, b) a = max((ll)a, (ll)b)
+
+// alias g++='g++ -I/mnt/c/Users/Owner/Desktop/ac-library'
+using ll = long long;
+using ld = long double;
+using vs = vector<ll>;
+using graph = vector<vector<int>>;
+using Graph = vector<vector<ll>>;
+using P = pair<ll, ll>;
+const int SIZE = 100005;
+const int inf = 1000000000;
+const int modi = 1000000007;
+const long long INF = 1000000000000000;
+const long long modl = 1000000007LL;
+const long long modll = 998244353LL;
+
+void yes() { cout << "Yes" << endl; }
+void no() { cout << "No" << endl; }
+void yn(bool t) {
+    if(t)
+        yes();
+    else
+        no();
+}
+void Yes() { cout << "YES" << endl; }
+void No() { cout << "NO" << endl; }
+void YN(bool t) {
+    if(t)
+        Yes();
+    else
+        No();
+}
+
+void pri(ll a) { cout << a << endl; }
+void spri(string a) { cout << a << endl; }
+void priV(vector<ll> &vec) {
+    for(size_t i = 0; i < vec.size(); i++) {
+        cout << vec[i] << ":";
+    }
+    cout << endl;
+}
+
+vector<int> dx = {1, 0, -1, 0};
+vector<int> dy = {0, 1, 0, -1};
+const string ALP = "ABCDEFGHIkkKLMNOPQRSTUVWXYZ";
+const string alp = "abcdefghijklmnopqrstuvwxyz";
+
+using mint = modint998244353;
+// using mint = modint1000000007;
+// cout << fixed << setprecision(12);
+
+int main(void) {
+    fio();
+    int Q;
+    cin >> Q;
+    vector<ll> c(Q), x(Q);
+
+    int i = 0;
+    while(Q--) {
+        ll q;
+        cin >> q;
+        if(q == 1) {
+            ll xx, cc;
+            cin >> xx >> cc;
+            x.push_back(xx);
+            c.push_back(cc);
+        }
+        if(q == 2) {
+            ll cc;
+            cin >> cc;
+            ll ans = 0;
+            while(cc > 0) {
+                ans += (x[i] * min(c[i], cc));
+                if(cc > c[i]) {
+                    cc -= c[i];
+                    i++;
+                } else {
+                    c[i] -= cc;
+                    cc = 0;
+                    break;
+                }
+                // cout << p[i].first << ":";
+                // cout << p[i].first << ":";
+                // cout << i << endl;
+            }
+            cout << ans << endl;
+        }
+        // cout << p[0].first << " " << p[0].second << endl;
+    }
+}
