64e737878ebba687992a705c26ab40f087fcfea7
sisi0808 <sivawqoqpqwu@gmail.com>
2022-04-17 09:30:17 +0900

    typical90_a

---
+++ typical90/a.cpp
@@ -0,0 +1,106 @@
+#include <bits/stdc++.h>
+/* ACLのライブラリを追加*/
+#include <atcoder/all>
+using namespace atcoder;
+
+#define fio()         \
+    cin.tie(nullptr); \
+    ios::sync_with_stdio(false);
+using namespace std;
+#define rep(i, n) for(int i = 0; i < int(n); ++i)
+#define rrep(i, n) for(int i = n; i >= 0; --i)
+#define repp(i, n, m) for(int i = m; i < int(n); ++i)
+#define fore(i_in, a) for(auto &i_in : a)
+#define ALL(v) (v).begin(), (v).end()
+#define RALL(v) (v).rbegin(), (v).rend()
+#define chmin(a, b) a = min((ll)a, (ll)b)
+#define chmax(a, b) a = max((ll)a, (ll)b)
+
+// alias g++='g++ -I/mnt/c/Users/Owner/Desktop/ac-library'
+using ll = long long;
+using ld = long double;
+using vs = vector<ll>;
+using graph = vector<vector<int>>;
+using Graph = vector<vector<ll>>;
+using P = pair<ll, ll>;
+const int SIZE = 100005;
+const int inf = 1000000000;
+const int modi = 1000000007;
+const long long INF = 1000000000000000;
+const long long modl = 1000000007LL;
+const long long modll = 998244353LL;
+
+void yes() { cout << "Yes" << endl; }
+void no() { cout << "No" << endl; }
+void yn(bool t) {
+    if(t)
+        yes();
+    else
+        no();
+}
+void Yes() { cout << "YES" << endl; }
+void No() { cout << "NO" << endl; }
+void YN(bool t) {
+    if(t)
+        Yes();
+    else
+        No();
+}
+
+void pri(ll a) { cout << a << endl; }
+void spri(string a) { cout << a << endl; }
+void priV(vector<ll> &vec) {
+    for(size_t i = 0; i < vec.size(); i++) {
+        cout << vec[i] << ":";
+    }
+    cout << endl;
+}
+
+vector<int> dx = {1, 0, -1, 0};
+vector<int> dy = {0, 1, 0, -1};
+const string ALP = "ABCDEFGHIkkKLMNOPQRSTUVWXYZ";
+const string alp = "abcdefghijklmnopqrstuvwxyz";
+
+using mint = modint998244353;
+// using mint = modint1000000007;
+// cout << fixed << setprecision(12);
+
+ll n, l, k;
+vs a;
+
+bool f(ll m) {
+    ll kk = 0;
+    ll sum_l = 0;
+    rep(i, n) {
+        if(a[i] - sum_l >= m) {
+            sum_l = a[i];
+            kk++;
+        }
+    }
+    if(l - sum_l >= m)
+        kk++;
+    if(kk > k)
+        return true;
+    else
+        return false;
+}
+
+int main(void) {
+    fio();
+    cin >> n >> l >> k;
+    a.resize(n);
+    rep(i, n) cin >> a[i];
+
+    ll ok = 0;
+    ll ng = INF;
+    while(ng - ok > 1) {
+        ll m = (ok + ng) / 2;
+        // cout << m << ":";
+        /* m以上の長さで分割可能ならば */
+        if(f(m))
+            ok = m;
+        else
+            ng = m;
+    }
+    cout << ok << endl;
+}
