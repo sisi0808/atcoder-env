e6e21cceadb6eded29138fcace9e0d4da74563ea
sisi0808 <sivawqoqpqwu@gmail.com>
2022-04-27 10:13:08 +0900

    typical90_u

---
+++ typical90/u.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+/* ACLのライブラリを追加*/
+#include <atcoder/all>
+using namespace atcoder;
+
+#define fio()         \
+    cin.tie(nullptr); \
+    ios::sync_with_stdio(false);
+using namespace std;
+#define rep(i, n) for(int i = 0; i < int(n); ++i)
+#define rrep(i, n) for(int i = n; i >= 0; --i)
+#define repp(i, n, m) for(int i = m; i < int(n); ++i)
+#define fore(i_in, a) for(auto &i_in : a)
+#define ALL(v) (v).begin(), (v).end()
+#define RALL(v) (v).rbegin(), (v).rend()
+#define chmin(a, b) a = min((ll)a, (ll)b)
+#define chmax(a, b) a = max((ll)a, (ll)b)
+
+// alias g++='g++ -I/mnt/c/Users/Owner/Desktop/ac-library'
+using ll = long long;
+using ld = long double;
+using vs = vector<ll>;
+using graph = vector<vector<int>>;
+using Graph = vector<vector<ll>>;
+using P = pair<ll, ll>;
+const int SIZE = 100005;
+const int inf = 1000000000;
+const int modi = 1000000007;
+const long long INF = 1000000000000000;
+const long long modl = 1000000007LL;
+const long long modll = 998244353LL;
+
+void yes() { cout << "Yes" << endl; }
+void no() { cout << "No" << endl; }
+void yn(bool t) {
+    if(t)
+        yes();
+    else
+        no();
+}
+void Yes() { cout << "YES" << endl; }
+void No() { cout << "NO" << endl; }
+void YN(bool t) {
+    if(t)
+        Yes();
+    else
+        No();
+}
+
+void pri(ll a) { cout << a << endl; }
+void spri(string a) { cout << a << endl; }
+void priV(vector<ll> &vec) {
+    for(size_t i = 0; i < vec.size(); i++) {
+        cout << vec[i] << ":";
+    }
+    cout << endl;
+}
+
+vector<int> dx = {1, 0, -1, 0};
+vector<int> dy = {0, 1, 0, -1};
+const string ALP = "ABCDEFGHIkkKLMNOPQRSTUVWXYZ";
+const string alp = "abcdefghijklmnopqrstuvwxyz";
+
+using mint = modint998244353;
+// using mint = modint1000000007;
+// cout << fixed << setprecision(12);
+
+int main(void) {
+    fio();
+    int n, m;
+    cin >> n >> m;
+    scc_graph g(n);
+
+    rep(i, m) {
+        int a, b;
+        cin >> a >> b;
+        g.add_edge(a - 1, b - 1);
+    }
+    ll ans = 0;
+    for(auto v : g.scc()) {
+        ans += v.size() * (v.size() - 1) / 2;
+    }
+    pri(ans);
+}
