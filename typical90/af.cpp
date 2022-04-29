ba6ee29409dbc745693cf3d3abfa3c096457556a
sisi0808 <sivawqoqpqwu@gmail.com>
2022-04-30 08:47:28 +0900

    typical90_af

---
+++ typical90/af.cpp
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+/* ACLのライブラリを追加*/
+#include <atcoder/all>
+using namespace atcoder;
+
+#define fio()         \
+    cin.tie(nullptr); \
+    ios::sync_with_stdio(false);
+using namespace std;
+#define rep(i, n) for(int i = 0; i < int(n); ++i)
+#define rrep(i, n) for(int i = n; i >= 0; --i)
+#define repp(i, n, m) for(int i = m; i < int(n); ++i)
+#define fore(i_in, a) for(auto &i_in : a)
+#define ALL(v) (v).begin(), (v).end()
+#define RALL(v) (v).rbegin(), (v).rend()
+#define chmin(a, b) a = min((ll)a, (ll)b)
+#define chmax(a, b) a = max((ll)a, (ll)b)
+
+// alias g++='g++ -I/mnt/c/Users/Owner/Desktop/ac-library'
+using ll = long long;
+using ld = long double;
+using vs = vector<ll>;
+using graph = vector<vector<int>>;
+using Graph = vector<vector<ll>>;
+using P = pair<ll, ll>;
+const int SIZE = 100005;
+const int inf = 1000000000;
+const int modi = 1000000007;
+const long long INF = 1000000000000000;
+const long long modl = 1000000007LL;
+const long long modll = 998244353LL;
+
+void yes() { cout << "Yes" << endl; }
+void no() { cout << "No" << endl; }
+void yn(bool t) {
+    if(t)
+        yes();
+    else
+        no();
+}
+void Yes() { cout << "YES" << endl; }
+void No() { cout << "NO" << endl; }
+void YN(bool t) {
+    if(t)
+        Yes();
+    else
+        No();
+}
+
+void pri(ll a) { cout << a << endl; }
+void spri(string a) { cout << a << endl; }
+void priV(vector<ll> &vec) {
+    for(size_t i = 0; i < vec.size(); i++) {
+        cout << vec[i] << ":";
+    }
+    cout << endl;
+}
+
+vector<int> dx = {1, 0, -1, 0};
+vector<int> dy = {0, 1, 0, -1};
+const string ALP = "ABCDEFGHIkkKLMNOPQRSTUVWXYZ";
+const string alp = "abcdefghijklmnopqrstuvwxyz";
+
+using mint = modint998244353;
+// using mint = modint1000000007;
+// cout << fixed << setprecision(12);
+
+int main(void) {
+    fio();
+    int n;
+    cin >> n;
+    Graph a(n, vector<ll>(n, 0));
+    vector<vector<bool>> threaten(n, vector<bool>(n, false));
+    rep(i, n) rep(j, n) cin >> a[i][j];
+    int m;
+    cin >> m;
+    rep(i, m) {
+        int a, b;
+        cin >> a >> b;
+        a--;
+        b--;
+        threaten[a][b] = true;
+        threaten[b][a] = true;
+    }
+
+    vector<int> b(n);
+    iota(ALL(b), 0);
+    ll ans = LLONG_MAX;
+    do {
+        ll _ans = 0;
+        bool f = true;
+        rep(i, n) {
+            _ans += a[b[i]][i];
+            if(i != 0 && threaten[b[i]][b[i - 1]])
+                f = false;
+        }
+        if(f)
+            chmin(ans, _ans);
+
+    } while(next_permutation(ALL(b)));
+
+    cout << (ans != LLONG_MAX ? ans : -1) << endl;
+}
