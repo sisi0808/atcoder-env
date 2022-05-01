5ac5b1b58fa57adf9c761a2f4f2a3174425b620f
sisi0808 <sivawqoqpqwu@gmail.com>
2022-05-01 12:38:47 +0900

    Update delete_compiled_file.py

--- delete_compiled_file.py
+++ delete_compiled_file.py
@@ -1,13 +1,18 @@
 import os
 import glob
+import re
 
 
-def remove_glob(pathname, recursive=True):
+def remove_glob(pathname, regex, recursive=True):
     for p in glob.glob(pathname, recursive=recursive):
-        if os.path.isfile(p):
+        if os.path.isfile(p) and re.search(regex, p):
             os.remove(p)
 
 
 if __name__ == "__main__":
-    pathname = "./**/?"
-    remove_glob(pathname)
+    # If you want to add target dir,
+    # you should add contest_name
+    contest_dir = ["abc", "typical90", "dp"]
+    regex = r"^[^\.]+$"
+    for pathname in contest_dir:
+        remove_glob(pathname + "/**", regex)
