dd2b34f31e6bb830255d7e1f4be394041386a5ac
sisi0808 <sivawqoqpqwu@gmail.com>
2022-05-21 09:48:36 +0900

    typical90_bd

--- typical90/bd.cpp
+++ typical90/bd.cpp
@@ -67,4 +67,48 @@ using mint = modint998244353;
 
 int main(void) {
     fio();
+    int n, s;
+    cin >> n >> s;
+    vs a(n), b(n);
+    rep(i, n) cin >> a[i] >> b[i];
+
+    vector<vector<bool>> dp(n + 1, vector<bool>(s + 1));
+    dp[0][0] = true;
+
+    rep(i, n) {
+        repp(j, s + 1, 1) {
+            /* Aを買う場合 */
+            if(j - a[i] >= 0 && dp[i][j - a[i]]) {
+                dp[i + 1][j] = true;
+            }
+            if(j - b[i] >= 0 && dp[i][j - b[i]]) {
+                /* Bを買う場合 */
+                dp[i + 1][j] = true;
+            }
+        }
+    }
+    /* 方法が存在しない場合 */
+    if(!dp[n][s]) {
+        cout << "Impossible" << endl;
+        return 0;
+    }
+
+    /* 方法が存在する場合 */
+    string ans = "";
+    for(int i = n - 1; i >= 0; i--) {
+        int ss = 0;
+        if(s - a[i] >= 0 && dp[i][s - a[i]]) {
+            ans += "A";
+            ss = a[i];
+        } else if(s - b[i] >= 0 && dp[i][s - b[i]]) {
+            ans += "B";
+            ss = b[i];
+        }
+
+        s -= ss;
+    }
+
+    reverse(ALL(ans));
+
+    cout << ans << endl;
 }
