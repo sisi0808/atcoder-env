46c46f129ca5ee1ce16d553b8692a445f03e1928
sisi0808 <sivawqoqpqwu@gmail.com>
2022-05-26 09:15:35 +0900

    typical90_ae

--- typical90/ae.cpp
+++ typical90/ae.cpp
@@ -65,6 +65,51 @@ using mint = modint998244353;
 // using mint = modint1000000007;
 // cout << fixed << setprecision(12);
 
-int main(void) {
-    fio();
+int N;
+int A[1 << 18], B[1 << 18];
+
+int grundy[55][1555];
+
+void init() {
+    for(int i = 0; i <= 50; i++) {
+        for(int j = 0; j <= 1500; j++) {
+            /* 初期化 */
+            int mex[1555];
+            for(int k = 0; k < 1555; k++) mex[k] = 0;
+
+            if(i >= 1) {
+                mex[grundy[i - 1][j + i]] = 1;
+            }
+            if(j >= 2) {
+                for(int k = 1; k <= (j / 2); k++) mex[grundy[i][j - k]] = 1;
+            }
+            for(int k = 0; k < 1555; k++) {
+                if(mex[k] == 0) {
+                    grundy[i][j] = k;
+                    break;
+                }
+            }
+        }
+    }
+}
+
+int main() {
+    // Step #1. Input
+    cin >> N;
+    for(int i = 1; i <= N; i++) cin >> A[i];
+    for(int i = 1; i <= N; i++) cin >> B[i];
+
+    // Step #2. Initialize
+    init();
+
+    // Step #3. Get Answer
+    int sum_xor = 0;
+    for(int i = 1; i <= N; i++) {
+        sum_xor ^= grundy[A[i]][B[i]];
+    }
+
+    // Step #4. Output
+    if(sum_xor != 0) cout << "First" << endl;
+    if(sum_xor == 0) cout << "Second" << endl;
+    return 0;
 }
