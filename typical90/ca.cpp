052134b1c36258b5dc883a46f3630515be9a7328
sisi0808 <sivawqoqpqwu@gmail.com>
2022-06-09 08:17:24 +0900

    typical90_ca

--- typical90/ca.cpp
+++ typical90/ca.cpp
@@ -65,6 +65,35 @@ using mint = modint998244353;
 // using mint = modint1000000007;
 // cout << fixed << setprecision(12);
 
+/* 左上から順番に取り組んでいく*/
+
 int main(void) {
     fio();
+    int h, w;
+    cin >> h >> w;
+    graph a(h, vector<int>(w));
+    graph b(h, vector<int>(w));
+    rep(i, h) rep(j, w) cin >> a[i][j];
+    rep(i, h) rep(j, w) cin >> b[i][j];
+
+    ll ans = 0;
+    rep(i, h - 1) {
+        rep(j, w - 1) {
+            if(a[i][j] != b[i][j]) {
+                int d = b[i][j] - a[i][j];
+                a[i][j] += d;
+                a[i + 1][j] += d;
+                a[i][j + 1] += d;
+                a[i + 1][j + 1] += d;
+                ans += abs(d);
+            }
+        }
+    }
+
+    if(a == b) {
+        yes();
+        pri(ans);
+    } else {
+        no();
+    }
 }
