7a1852062015c440ac0b0b51ecb9c7af5201db73
sisi0808 <sivawqoqpqwu@gmail.com>
2022-06-15 10:22:53 +0900

    typical90_ci

--- typical90/ci.cpp
+++ typical90/ci.cpp
@@ -26,7 +26,8 @@ using P = pair<ll, ll>;
 const int SIZE = 100005;
 const int inf = 1000000000;
 const int modi = 1000000007;
-const long long INF = 1000000000000000;
+const long long INF = 1000000000000000LL;
+const long long _INF = 1000000000000000000LL;
 const long long modl = 1000000007LL;
 const long long modll = 998244353LL;
 
@@ -64,7 +65,51 @@ const string alp = "abcdefghijklmnopqrstuvwxyz";
 using mint = modint998244353;
 // using mint = modint1000000007;
 // cout << fixed << setprecision(12);
+ll n, p, k;
+Graph a;
+
+ll f(ll mid) {
+    ll dist[41][41];
+    // 直接つながっているものはその辺のコストで初期化
+    //、開始と終了の点が同じ場合、コストは0
+    // それ以外はinfに設定
+
+    rep(i, n) rep(j, n) dist[i][j] = a[i][j] == -1 ? mid : a[i][j];
+
+    rep(h, n) rep(i, n) rep(j, n) chmin(dist[i][j], dist[i][h] + dist[h][j]);
+
+    int cnt = 0;
+    rep(i, n) repp(j, n, i + 1) if(dist[i][j] <= p) cnt++;
+
+    return cnt;
+}
+
+ll get_border(ll cnt) {
+    /* どんな二分探索でもここの書き方を変えずにできる！ */
+    /* okが最小値でも、最大値でも同じ書き方 */
+    ll left = 0;
+    ll right = 5 * _INF;
+    while(right - left > 1) {
+        ll mid = left + (right - left) / 2;
+        if(f(mid) <= cnt) {
+            right = mid;
+        } else left = mid;
+    }
+    // cout << left << " " << right << endl;
+    return right;
+}
 
 int main(void) {
     fio();
+    cin >> n >> p >> k;
+    a.resize(n, vector<ll>(n));
+    rep(i, n) rep(j, n) cin >> a[i][j];
+
+    ll l_border = get_border(k);
+    ll r_border = get_border(k - 1);
+
+    // cout << l_border << " " << r_border << endl;
+
+    if(r_border - l_border >= 2 * _INF) cout << "Infinity" << endl;
+    else cout << r_border - l_border << endl;
 }
