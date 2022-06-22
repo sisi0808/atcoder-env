bef0e8992ff20980cbd3e4f634a146ac0f8a6258
sisi0808 <sivawqoqpqwu@gmail.com>
2022-06-22 10:04:45 +0900

    abc185_f

---
+++ abc/185/f.cpp
@@ -0,0 +1,117 @@
+#include <bits/stdc++.h>
+/* ACLのライブラリを追加*/
+#include <atcoder/all>
+using namespace atcoder;
+
+#define fio()         \
+    cin.tie(nullptr); \
+    ios::sync_with_stdio(false);
+using namespace std;
+#define rep(i, n) for(int i = 0; i < int(n); ++i)
+#define rrep(i, n) for(int i = n; i >= 0; --i)
+#define repp(i, n, m) for(int i = m; i < int(n); ++i)
+#define fore(i_in, a) for(auto &i_in : a)
+#define ALL(v) (v).begin(), (v).end()
+#define RALL(v) (v).rbegin(), (v).rend()
+#define chmin(a, b) a = min((ll)a, (ll)b)
+#define chmax(a, b) a = max((ll)a, (ll)b)
+
+#define pb push_back
+#define pf push_front
+
+#define fi first
+#define se second
+
+// alias g++='g++ -I/mnt/c/Users/Owner/Desktop/ac-library'
+using ll = long long;
+using ld = long double;
+using ivec = vector<int>;
+using lvec = vector<ll>;
+using graph = vector<vector<int>>;
+using Graph = vector<vector<ll>>;
+using P = pair<ll, ll>;
+const int SIZE = 100005;
+const int inf = 1 << 31;
+const int _inf = 1000000000;
+const int modi = 1000000007;
+const long long INF = 1LL << 62;
+const long long _INF = 1000000000000000000LL;
+const long long modl = 1000000007LL;
+const long long modll = 998244353LL;
+
+void yes() { cout << "Yes" << endl; }
+void no() { cout << "No" << endl; }
+void yn(bool t) {
+    if(t)
+        yes();
+    else
+        no();
+}
+void Yes() { cout << "YES" << endl; }
+void No() { cout << "NO" << endl; }
+void YN(bool t) {
+    if(t)
+        Yes();
+    else
+        No();
+}
+
+void pri(ll a) { cout << a << endl; }
+void spri(string a) { cout << a << endl; }
+void priV(vector<ll> &vec) {
+    for(size_t i = 0; i < vec.size(); i++) {
+        cout << vec[i] << ":";
+    }
+    cout << endl;
+}
+
+vector<int> dx = {1, 0, -1, 0};
+vector<int> dy = {0, 1, 0, -1};
+const string ALP = "ABCDEFGHIkkKLMNOPQRSTUVWXYZ";
+const string alp = "abcdefghijklmnopqrstuvwxyz";
+
+// using mint = modint998244353;
+using mint = modint1000000007;
+// cout << fixed << setprecision(12);
+
+/*
+１が偶数個ならば、出力は0
+１が奇数個ならば、出力は1
+*/
+
+int main(void) {
+    fio();
+    int n, q;
+    cin >> n >> q;
+    lvec a(n);
+    rep(i, n) cin >> a[i];
+    vector<fenwick_tree<int>> fw(32, fenwick_tree<int>(n + 1));
+
+    rep(i, n) {
+        rep(j, 32) {
+            if(a[i] & (1 << j)) fw[j].add(i, 1);
+        }
+    }
+
+    while(q--) {
+        int t;
+        cin >> t;
+        ll x, y;
+        cin >> x >> y;
+        x--;
+        if(t == 1) {
+            rep(j, 32) {
+                if(y & (1 << j)) fw[j].add(x, 1);
+            }
+
+        } else { // t==2
+            ll ans = 0;
+            rep(j, 32) {
+                if(fw[j].sum(x, y) % 2 == 1) {
+                    ans += (1 << j);
+                }
+            }
+            pri(ans);
+        }
+    }
+}
