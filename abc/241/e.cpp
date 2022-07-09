e819c652d8450bb3320c384a9374fc2efbe4c4e3
sisi0808 <sivawqoqpqwu@gmail.com>
2022-07-09 12:10:20 +0900

    abc241_e

--- abc/241/e.cpp
+++ abc/241/e.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 /* ACLのライブラリを追加*/
-#include<atcoder/all>
+#include <atcoder/all>
 using namespace atcoder;
 
 #define fio()         \
@@ -63,6 +63,41 @@ const string alp = "abcdefghijklmnopqrstuvwxyz";
 graph G;
 using mint = modint998244353;
 
-int main(void){
+// これも鳩ノ巣
+
+int main(void) {
     fio();
+    ll n, k;
+    cin >> n >> k;
+    vector<ll> a(n);
+    rep(i, n) cin >> a[i];
+
+    vector<ll> box(n + 1);     // i番目の数
+    vector<ll> pre(n + 1, -1); // 配列のそのidxを見たか && それは何番目か
+    int loop_start;            // ループ開始地点
+    int loop_end;              // ループの終わり
+
+    box[0] = 0;
+    pre[0] = 0;
+    rep(i, n) {
+        box[i + 1] = box[i] + a[box[i] % n];
+        if(pre[box[i + 1] % n] != -1) { // 既にそのidxを見ていた場合
+            loop_start = pre[box[i + 1] % n];
+            loop_end = i + 1;
+            break;
+        }
+        pre[box[i + 1] % n] = i + 1;
+    }
+    // rep(i, n + 1) cout << pre[i] << endl;
+
+    ll ans = 0;
+    if(k < loop_start) ans = box[k];
+    else {
+        int loop_length = loop_end - loop_start;
+        int loop_plus = (k - loop_start - 1) % loop_length;
+        ll loop_cnt = (k - loop_start - 1) / loop_length;
+        ans += box[loop_start + loop_plus + 1];
+        ans += (box[loop_end] - box[loop_start]) * loop_cnt;
+    }
+    cout << ans << endl;
 }
