438ecae64cae39530215d0e8ffac57a34ea5fcf5
sisi0808 <sivawqoqpqwu@gmail.com>
2022-07-14 11:44:26 +0900

    abc169_a-d

---
+++ abc/169/d.cpp
@@ -0,0 +1,106 @@
+#include <bits/stdc++.h>
+/* ACLのライブラリを追加*/
+#include <atcoder/all>
+using namespace atcoder;
+
+#define fio()         \
+    cin.tie(nullptr); \
+    ios::sync_with_stdio(false);
+using namespace std;
+#define rep(i, n) for(int i = 0; i < int(n); ++i)
+#define rrep(i, n) for(int i = n; i >= 0; --i)
+#define repp(i, n, m) for(int i = m; i < int(n); ++i)
+#define fore(i_in, a) for(auto &i_in : a)
+#define ALL(v) (v).begin(), (v).end()
+#define RALL(v) (v).rbegin(), (v).rend()
+#define chmin(a, b) a = min((ll)a, (ll)b)
+#define chmax(a, b) a = max((ll)a, (ll)b)
+
+#define pb push_back
+#define pf push_front
+
+#define fi first
+#define se second
+
+// alias g++='g++ -I/mnt/c/Users/Owner/Desktop/ac-library'
+using ll = long long;
+using ld = long double;
+using ivec = vector<int>;
+using lvec = vector<ll>;
+using graph = vector<vector<int>>;
+using Graph = vector<vector<ll>>;
+using P = pair<ll, ll>;
+const int SIZE = 100005;
+const int inf = 1 << 31;
+const int _inf = 1000000000;
+const int modi = 1000000007;
+const long long INF = 1LL << 62;
+const long long _INF = 1000000000000000000LL;
+const long long modl = 1000000007LL;
+const long long modll = 998244353LL;
+
+void yes() { cout << "Yes" << endl; }
+void no() { cout << "No" << endl; }
+void yn(bool t) {
+    if(t)
+        yes();
+    else
+        no();
+}
+void Yes() { cout << "YES" << endl; }
+void No() { cout << "NO" << endl; }
+void YN(bool t) {
+    if(t)
+        Yes();
+    else
+        No();
+}
+
+vector<int> dx = {1, 0, -1, 0};
+vector<int> dy = {0, 1, 0, -1};
+const string ALP = "ABCDEFGHIkkKLMNOPQRSTUVWXYZ";
+const string alp = "abcdefghijklmnopqrstuvwxyz";
+
+// using mint = modint998244353;
+using mint = modint1000000007;
+// cout << fixed << setprecision(12);
+
+/*
+
+素数の累乗を羅列する
+
+まずは素因数分解
+
+*/
+
+vector<ll> pfact(ll x) {
+    vector<ll> res;
+    for(ll i = 2; i * i <= x; i++) {
+        while(x % i == 0) {
+            x /= i;
+            res.push_back(i);
+        }
+    }
+    if(x != 1) res.push_back(x);
+    return res;
+}
+
+int main(void) {
+    fio();
+    ll n;
+    cin >> n;
+    map<ll, ll> mp;
+    for(auto x : pfact(n)) {
+        mp[x]++;
+    }
+
+    ll ans = 0;
+    vector<int> aa(10);
+    aa[0] = 1;
+    rep(i, 9) aa[i + 1] = aa[i] + (i + 2);
+    for(auto [a, b] : mp) {
+        int idx = upper_bound(ALL(aa), b) - aa.begin();
+        ans += idx;
+    }
+    cout << ans << endl;
+}
