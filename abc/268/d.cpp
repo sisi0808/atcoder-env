8a9a8684ff0a6d4752415c9d345d8a7e31964dad
sisi0808 <sivawqoqpqwu@gmail.com>
2022-09-10 22:10:29 +0900

    abc268_a-e

---
+++ abc/268/d.cpp
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h>
+/* ACLのライブラリを追加*/
+#include <atcoder/all>
+using namespace atcoder;
+
+#define fio()         \
+    cin.tie(nullptr); \
+    ios::sync_with_stdio(false);
+using namespace std;
+#define rep(i, n) for(int i = 0; i < int(n); ++i)
+#define rrep(i, n) for(int i = n; i >= 0; --i)
+#define repp(i, n, m) for(int i = m; i < int(n); ++i)
+#define fore(i_in, a) for(auto &i_in : a)
+#define ALL(v) (v).begin(), (v).end()
+#define RALL(v) (v).rbegin(), (v).rend()
+#define chmin(a, b) a = min((ll)a, (ll)b)
+#define chmax(a, b) a = max((ll)a, (ll)b)
+
+#define pb push_back
+#define pf push_front
+
+#define fi first
+#define se second
+
+// alias g++='g++ -I/mnt/c/Users/Owner/Desktop/ac-library'
+using ll = long long;
+using ld = long double;
+using ivec = vector<int>;
+using lvec = vector<ll>;
+using graph = vector<vector<int>>;
+using Graph = vector<vector<ll>>;
+using P = pair<ll, ll>;
+const int SIZE = 100005;
+const int inf = 1 << 30;
+const int _inf = 1000000000;
+const int modi = 1000000007;
+const long long INF = 1LL << 62;
+const long long _INF = 1000000000000000000LL;
+const long long modl = 1000000007LL;
+const long long modll = 998244353LL;
+
+void yes() { cout << "Yes" << endl; }
+void no() { cout << "No" << endl; }
+void yn(bool t) {
+    if(t)
+        yes();
+    else
+        no();
+}
+void Yes() { cout << "YES" << endl; }
+void No() { cout << "NO" << endl; }
+void YN(bool t) {
+    if(t)
+        Yes();
+    else
+        No();
+}
+
+vector<int> dx = {1, 0, -1, 0};
+vector<int> dy = {0, 1, 0, -1};
+const string ALP = "ABCDEFGHIkkKLMNOPQRSTUVWXYZ";
+const string alp = "abcdefghijklmnopqrstuvwxyz";
+
+// using mint = modint998244353;
+using mint = modint1000000007;
+// cout << fixed << setprecision(12);
+
+ll n, m;
+vector<string> s;
+vector<string> t;
+set<string> st;
+
+bool flag = false;
+void dfs(string c, int idx) {
+    repp(i, 16 - c.size() + 1, 1) {
+        if(idx + 1 < n && c.size() + i + s[idx + 1].size() <= 16) dfs(c + string(i, '_') + s[idx + 1], idx + 1);
+    }
+    if(!flag && c.size() >= 3 && idx + 1 == n && !st.count(c)) {
+        cout << c << endl;
+        flag = true;
+        return;
+    }
+}
+
+int main(void) {
+    fio();
+    cin >> n >> m;
+    s.resize(n);
+    t.resize(m);
+    rep(i, n) cin >> s[i];
+    rep(i, m) cin >> t[i];
+    rep(i, m) st.insert(t[i]);
+    sort(ALL(s));
+
+    do {
+        dfs(s[0], 0);
+        if(flag) {
+            return 0;
+        }
+    } while(next_permutation(ALL(s)));
+    cout << -1 << endl;
+}
