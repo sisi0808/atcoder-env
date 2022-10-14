5e93c5d97a377055c1e713f9f914121dcd73e12d
sisi0808 <sivawqoqpqwu@gmail.com>
2022-10-14 11:42:47 +0900

    abc140_d

---
+++ abc/140/d.cpp
@@ -0,0 +1,95 @@
+#include <bits/stdc++.h>
+/* ACLのライブラリを追加*/
+#include <atcoder/all>
+using namespace atcoder;
+
+#define fio()         \
+    cin.tie(nullptr); \
+    ios::sync_with_stdio(false);
+using namespace std;
+#define rep(i, n) for(int i = 0; i < int(n); ++i)
+#define rrep(i, n, m) for(int i = n; i >= m; --i)
+#define repp(i, n, m) for(int i = m; i < int(n); ++i)
+#define fore(i_in, a) for(auto &i_in : a)
+#define ALL(v) (v).begin(), (v).end()
+#define RALL(v) (v).rbegin(), (v).rend()
+#define chmin(a, b) a = min((ll)a, (ll)b)
+#define chmax(a, b) a = max((ll)a, (ll)b)
+
+#define pb push_back
+#define pf push_front
+
+#define fi first
+#define se second
+
+// alias g++='g++ -I/mnt/c/Users/Owner/Desktop/ac-library'
+using ll = long long;
+using ld = long double;
+using ivec = vector<int>;
+using lvec = vector<ll>;
+using graph = vector<vector<int>>;
+using Graph = vector<vector<ll>>;
+using P = pair<ll, ll>;
+const int SIZE = 100005;
+const int inf = 1 << 30;
+const int _inf = 1000000000;
+const int modi = 1000000007;
+const long long INF = 1LL << 62;
+const long long _INF = 1000000000000000000LL;
+const long long modl = 1000000007LL;
+const long long modll = 998244353LL;
+
+void yes() { cout << "Yes" << endl; }
+void no() { cout << "No" << endl; }
+void yn(bool t) {
+    if(t)
+        yes();
+    else
+        no();
+}
+void Yes() { cout << "YES" << endl; }
+void No() { cout << "NO" << endl; }
+void YN(bool t) {
+    if(t)
+        Yes();
+    else
+        No();
+}
+
+vector<int> dx = {1, 0, -1, 0};
+vector<int> dy = {0, 1, 0, -1};
+const string ALP = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const string alp = "abcdefghijklmnopqrstuvwxyz";
+
+// using mint = modint998244353;
+using mint = modint1000000007;
+// cout << fixed << setprecision(12);
+
+/*
+ * aabbbbbbaaという集団で最長の長さの箇所を反転する
+ * 貪欲ではダメそう
+ * どうやら、反転箇所が端なのも良くない
+ *
+ *
+ * 一回の操作で、最大2個答えを増やすことが可能
+ * 出来ないのは,グループが3しかない時
+ */
+
+int main(void) {
+    fio();
+    ll n, k;
+    cin >> n >> k;
+    string s;
+    cin >> s;
+
+    ll cnt = 1;
+    repp(i, n, 1) if(s[i] != s[i - 1]) cnt++;
+
+    // /* k回の操作 */
+    rep(i, k) {
+        if(cnt >= 3) cnt -= 2;
+        else cnt--;
+    }
+
+    cout << n - max(1LL, cnt) << endl;
+}
