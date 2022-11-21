c02b0e56ed63feef8b0ae38b3b20b6e490abfe91
sisi0808 <sivawqoqpqwu@gmail.com>
2022-11-21 14:32:09 +0900

    abc125_a-b,d

---
+++ abc/125/d.cpp
@@ -0,0 +1,92 @@
+#include <bits/stdc++.h>
+/* ACLのライブラリを追加*/
+#include <atcoder/all>
+using namespace atcoder;
+
+#define fio()         \
+    cin.tie(nullptr); \
+    ios::sync_with_stdio(false);
+using namespace std;
+#define rep(i, n) for(int i = 0; i < int(n); ++i)
+#define rrep(i, n, m) for(int i = n; i >= m; --i)
+#define repp(i, n, m) for(int i = m; i < int(n); ++i)
+#define fore(i_in, a) for(auto &i_in : a)
+#define ALL(v) (v).begin(), (v).end()
+#define RALL(v) (v).rbegin(), (v).rend()
+#define chmin(a, b) a = min((ll)a, (ll)b)
+#define chmax(a, b) a = max((ll)a, (ll)b)
+
+#define pb push_back
+#define pf push_front
+
+#define fi first
+#define se second
+
+// alias g++='g++ -I/mnt/c/Users/Owner/Desktop/ac-library'
+using ll = long long;
+using ld = long double;
+using ivec = vector<int>;
+using lvec = vector<ll>;
+using graph = vector<vector<int>>;
+using Graph = vector<vector<ll>>;
+using P = pair<ll, ll>;
+const int SIZE = 100005;
+const int inf = 1 << 30;
+const int _inf = 1000000000;
+const int modi = 1000000007;
+const long long INF = 1LL << 62;
+const long long _INF = 1000000000000000000LL;
+const long long modl = 1000000007LL;
+const long long modll = 998244353LL;
+
+void yes() { cout << "Yes" << endl; }
+void no() { cout << "No" << endl; }
+void yn(bool t) {
+    if(t)
+        yes();
+    else
+        no();
+}
+void Yes() { cout << "YES" << endl; }
+void No() { cout << "NO" << endl; }
+void YN(bool t) {
+    if(t)
+        Yes();
+    else
+        No();
+}
+
+vector<int> dx = {1, 0, -1, 0};
+vector<int> dy = {0, 1, 0, -1};
+const string ALP = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const string alp = "abcdefghijklmnopqrstuvwxyz";
+
+// using mint = modint998244353;
+using mint = modint1000000007;
+// cout << fixed << setprecision(12);
+
+/*
+* 一つの値を2回選ぶ事は何も選ばないことと同じ
+* 短期的に見ればミスでも、長期的に見れば正解のことがある(-3, 10, -4は全部処理)
+
+* 貪欲に行けるか？
+* 逆順はどうか
+*/
+
+int main(void) {
+    fio();
+    int n;
+    cin >> n;
+    vector<ll> a(n);
+    rep(i, n) cin >> a[i];
+
+    vector<vector<ll>> dp(n + 1, vector<ll>(2, -_INF));
+    dp[0][0] = 0;
+
+    /* 先頭をめくる時 */
+    rep(i, n - 1) {
+        dp[i + 1][0] = max(dp[i][0] + a[i], dp[i][1] - a[i]);
+        dp[i + 1][1] = max(dp[i][0] - a[i], dp[i][1] + a[i]);
+    }
+    cout << dp[n][0] << endl;
+}
