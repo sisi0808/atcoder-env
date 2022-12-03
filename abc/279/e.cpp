3a9c2e65aaa584b0e5af6a15a10828144c1c1057
sisi0808 <sivawqoqpqwu@gmail.com>
2022-12-03 20:05:39 +0900

    abc279_e

---
+++ abc/279/e.cpp
@@ -0,0 +1,108 @@
+#include <bits/stdc++.h>
+/* ACLのライブラリを追加*/
+#include <atcoder/all>
+using namespace atcoder;
+
+#define fio()         \
+    cin.tie(nullptr); \
+    ios::sync_with_stdio(false);
+using namespace std;
+#define rep(i, n) for(int i = 0; i < int(n); ++i)
+#define rrep(i, n, m) for(int i = n; i >= m; --i)
+#define repp(i, n, m) for(int i = m; i < int(n); ++i)
+#define fore(i_in, a) for(auto &i_in : a)
+#define ALL(v) (v).begin(), (v).end()
+#define RALL(v) (v).rbegin(), (v).rend()
+#define chmin(a, b) a = min((ll)a, (ll)b)
+#define chmax(a, b) a = max((ll)a, (ll)b)
+
+#define pb push_back
+#define pf push_front
+
+#define fi first
+#define se second
+
+// alias g++='g++ -I/mnt/c/Users/Owner/Desktop/ac-library'
+using ll = long long;
+using ld = long double;
+using ivec = vector<int>;
+using lvec = vector<ll>;
+using graph = vector<vector<int>>;
+using Graph = vector<vector<ll>>;
+using P = pair<ll, ll>;
+const int SIZE = 100005;
+const int inf = 1 << 30;
+const int _inf = 1000000000;
+const int modi = 1000000007;
+const long long INF = 1LL << 62;
+const long long _INF = 1000000000000000000LL;
+const long long modl = 1000000007LL;
+const long long modll = 998244353LL;
+
+void yes() { cout << "Yes" << endl; }
+void no() { cout << "No" << endl; }
+void yn(bool t) {
+    if(t)
+        yes();
+    else
+        no();
+}
+void Yes() { cout << "YES" << endl; }
+void No() { cout << "NO" << endl; }
+void YN(bool t) {
+    if(t)
+        Yes();
+    else
+        No();
+}
+
+vector<int> dx = {1, 0, -1, 0};
+vector<int> dy = {0, 1, 0, -1};
+const string ALP = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const string alp = "abcdefghijklmnopqrstuvwxyz";
+
+// using mint = modint998244353;
+using mint = modint1000000007;
+// cout << fixed << setprecision(12);
+
+/*
+* m-1回の操作を通して、1が最終的にどこに行くかを求める
+
+* dp or グラフ？
+*/
+
+int main(void) {
+    fio();
+    ll n, m;
+    cin >> n >> m;
+    vector<ll> a(m);
+    rep(i, m) cin >> a[i];
+
+    /* どのidxの数か、最終的にどこに移動するか */
+    vector<int> o(n);
+    iota(ALL(o), 1);
+
+    vector<P> p;
+
+    rep(i, m) {
+        swap(o[a[i] - 1], o[a[i]]);
+        p.pb({o[a[i] - 1], o[a[i]]});
+    }
+
+    /* どの数字がどのインデックスにあるか */
+    map<int, int> mp;
+    rep(i, n) {
+        mp[o[i]] = i + 1;
+    }
+
+    rep(i, m) {
+        auto [p1, p2] = p[i];
+        if(p1 == 1) {
+            cout << mp[p2] << endl;
+        } else if(p2 == 1) {
+            cout << mp[p1] << endl;
+        } else {
+            cout << mp[1] << endl;
+        }
+    }
+}
