424c45cfd6c87625999c76e22946056531855c6e
sisi0808 <sivawqoqpqwu@gmail.com>
2022-12-10 13:37:23 +0900

    abc114_a-d

---
+++ abc/114/d.cpp
@@ -0,0 +1,106 @@
+#include <bits/stdc++.h>
+/* ACLのライブラリを追加*/
+#include <atcoder/all>
+using namespace atcoder;
+
+#define fio()         \
+    cin.tie(nullptr); \
+    ios::sync_with_stdio(false);
+using namespace std;
+#define rep(i, n) for(int i = 0; i < int(n); ++i)
+#define rrep(i, n, m) for(int i = n; i >= m; --i)
+#define repp(i, n, m) for(int i = m; i < int(n); ++i)
+#define fore(i_in, a) for(auto &i_in : a)
+#define ALL(v) (v).begin(), (v).end()
+#define RALL(v) (v).rbegin(), (v).rend()
+#define chmin(a, b) a = min((ll)a, (ll)b)
+#define chmax(a, b) a = max((ll)a, (ll)b)
+
+#define pb push_back
+#define pf push_front
+
+#define fi first
+#define se second
+
+// alias g++='g++ -I/mnt/c/Users/Owner/Desktop/ac-library'
+using ll = long long;
+using ld = long double;
+using ivec = vector<int>;
+using lvec = vector<ll>;
+using graph = vector<vector<int>>;
+using Graph = vector<vector<ll>>;
+using P = pair<ll, ll>;
+const int SIZE = 100005;
+const int inf = 1 << 30;
+const int _inf = 1000000000;
+const int modi = 1000000007;
+const long long INF = 1LL << 62;
+const long long _INF = 1000000000000000000LL;
+const long long modl = 1000000007LL;
+const long long modll = 998244353LL;
+
+void yes() { cout << "Yes" << endl; }
+void no() { cout << "No" << endl; }
+void yn(bool t) {
+    if(t)
+        yes();
+    else
+        no();
+}
+void Yes() { cout << "YES" << endl; }
+void No() { cout << "NO" << endl; }
+void YN(bool t) {
+    if(t)
+        Yes();
+    else
+        No();
+}
+
+vector<int> dx = {1, 0, -1, 0};
+vector<int> dy = {0, 1, 0, -1};
+const string ALP = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const string alp = "abcdefghijklmnopqrstuvwxyz";
+
+// using mint = modint998244353;
+using mint = modint1000000007;
+// cout << fixed << setprecision(12);
+
+/*
+ * 恐らくは、初めの段階で1~100までの答えを出すことが出来る
+ * 基本的には約数側から計算しておくもの
+ */
+
+vector<ll> pfact(ll x) {
+    vector<ll> res;
+    for(ll i = 2; i * i <= x; i++) {
+        while(x % i == 0) {
+            x /= i;
+            res.push_back(i);
+        }
+    }
+    if(x != 1) res.push_back(x);
+    return res;
+}
+
+int main(void) {
+    fio();
+    int n;
+    cin >> n;
+
+    /* N!の因数の数を管理 */
+    map<ll, ll> mp;
+    repp(i, n + 1, 1) {
+        for(auto p : pfact(i)) mp[p]++;
+    }
+    ll ans = 0;
+
+    // 75
+    repp(i, n + 1, 2) if(74 <= mp[i]) ans++;
+    // 15*5
+    repp(i, n + 1, 2) repp(j, n + 1, 2) if(i != j and 14 <= mp[i] and 4 <= mp[j]) ans++;
+    // 3*25
+    repp(i, n + 1, 2) repp(j, n + 1, 2) if(i != j and 24 <= mp[i] and 2 <= mp[j]) ans++;
+    // 3*5*5
+    repp(i, n + 1, 2) repp(j, n + 1, 2) repp(k, n + 1, j + 1) if(i != j and i != k and 2 <= mp[i] and 4 <= mp[j] and 4 <= mp[k]) ans++;
+    cout << ans << endl;
+}
