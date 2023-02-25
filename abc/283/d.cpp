ba2bd45222cad66fb60861dbe6e36ad9d11109cd
sisi0808 <sivawqoqpqwu@gmail.com>
2023-02-25 12:45:20 +0900

    abc283_a-d

---
+++ abc/283/d.cpp
@@ -0,0 +1,96 @@
+#include <bits/stdc++.h>
+/* ACLのライブラリを追加*/
+#include <atcoder/all>
+using namespace atcoder;
+
+#define fio()         \
+    cin.tie(nullptr); \
+    ios::sync_with_stdio(false);
+using namespace std;
+#define rep(i, n) for(int i = 0; i < int(n); ++i)
+#define rrep(i, n, m) for(int i = n; i >= m; --i)
+#define repp(i, n, m) for(int i = m; i < int(n); ++i)
+#define fore(i_in, a) for(auto &i_in : a)
+#define ALL(v) (v).begin(), (v).end()
+#define RALL(v) (v).rbegin(), (v).rend()
+#define chmin(a, b) a = min((ll)a, (ll)b)
+#define chmax(a, b) a = max((ll)a, (ll)b)
+
+#define pb push_back
+#define pf push_front
+
+#define fi first
+#define se second
+
+// alias g++='g++ -I/mnt/c/Users/Owner/Desktop/ac-library'
+using ll = long long;
+using ld = long double;
+using ivec = vector<int>;
+using lvec = vector<ll>;
+using graph = vector<vector<int>>;
+using Graph = vector<vector<ll>>;
+using P = pair<ll, ll>;
+const int SIZE = 100005;
+const int inf = 1 << 30;
+const int _inf = 1000000000;
+const int modi = 1000000007;
+const long long INF = 1LL << 62;
+const long long _INF = 1000000000000000000LL;
+const long long modl = 1000000007LL;
+const long long modll = 998244353LL;
+
+void yes() { cout << "Yes" << endl; }
+void no() { cout << "No" << endl; }
+void yn(bool t) {
+    if(t)
+        yes();
+    else
+        no();
+}
+void Yes() { cout << "YES" << endl; }
+void No() { cout << "NO" << endl; }
+void YN(bool t) {
+    if(t)
+        Yes();
+    else
+        No();
+}
+
+vector<int> dx = {1, 0, -1, 0};
+vector<int> dy = {0, 1, 0, -1};
+const string ALP = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const string alp = "abcdefghijklmnopqrstuvwxyz";
+
+// using mint = modint998244353;
+using mint = modint1000000007;
+// cout << fixed << setprecision(12);
+
+/*
+ * カッコが閉じられた時に、カッコ内の文字をすべて取り除く処理を高速化する必要がある
+ * 入れる、取り出すという操作の２回分だけ出来ればよい
+ */
+
+int main(void) {
+    fio();
+    string s;
+    cin >> s;
+    vector<int> p(26);
+    vector<char> st;
+    rep(i, s.size()) {
+        st.pb(s[i]);
+        if(s[i] == ')') {
+            while(st.size() != 0 && st.back() != '(') {
+                p[st.back() - 'a']--;
+                st.pop_back();
+            }
+            if(st.size() != 0 && st.back() == '(') st.pop_back();
+        } else if(s[i] != '(') { // 英小文字
+            if(p[s[i] - 'a'] > 0) {
+                no();
+                return 0;
+            }
+            p[s[i] - 'a']++;
+        }
+    }
+    yes();
+}
