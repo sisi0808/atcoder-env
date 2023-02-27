b5539bd9cc9401012dbd8dd9817dca918f46a6de
sisi0808 <sivawqoqpqwu@gmail.com>
2023-02-27 11:35:52 +0900

    abc084_c

---
+++ abc/084/c.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+/* ACLのライブラリを追加*/
+#include <atcoder/all>
+using namespace atcoder;
+
+#define fio()         \
+    cin.tie(nullptr); \
+    ios::sync_with_stdio(false);
+using namespace std;
+#define rep(i, n) for(int i = 0; i < int(n); ++i)
+#define rrep(i, n, m) for(int i = n; i >= m; --i)
+#define repp(i, n, m) for(int i = m; i < int(n); ++i)
+#define fore(i_in, a) for(auto &i_in : a)
+#define ALL(v) (v).begin(), (v).end()
+#define RALL(v) (v).rbegin(), (v).rend()
+#define chmin(a, b) a = min((ll)a, (ll)b)
+#define chmax(a, b) a = max((ll)a, (ll)b)
+
+#define pb push_back
+#define pf push_front
+
+#define fi first
+#define se second
+
+// alias g++='g++ -I/mnt/c/Users/Owner/Desktop/ac-library'
+using ll = long long;
+using ld = long double;
+using ivec = vector<int>;
+using lvec = vector<ll>;
+using graph = vector<vector<int>>;
+using Graph = vector<vector<ll>>;
+using P = pair<ll, ll>;
+const int SIZE = 100005;
+const int inf = 1 << 30;
+const int _inf = 1000000000;
+const int modi = 1000000007;
+const long long INF = 1LL << 62;
+const long long _INF = 1000000000000000000LL;
+const long long modl = 1000000007LL;
+const long long modll = 998244353LL;
+
+void yes() { cout << "Yes" << endl; }
+void no() { cout << "No" << endl; }
+void yn(bool t) {
+    if(t)
+        yes();
+    else
+        no();
+}
+void Yes() { cout << "YES" << endl; }
+void No() { cout << "NO" << endl; }
+void YN(bool t) {
+    if(t)
+        Yes();
+    else
+        No();
+}
+
+vector<int> dx = {1, 0, -1, 0};
+vector<int> dy = {0, 1, 0, -1};
+const string ALP = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const string alp = "abcdefghijklmnopqrstuvwxyz";
+
+// using mint = modint998244353;
+using mint = modint1000000007;
+// cout << fixed << setprecision(12);
+
+int main(void) {
+    fio();
+    int n;
+    cin >> n;
+    vector<ll> c(n - 1), s(n - 1), f(n - 1);
+    rep(i, n - 1) cin >> c[i] >> s[i] >> f[i];
+
+    vector<ll> ans(n);
+
+    rep(i, n - 1) {
+        ll st = 0;
+        repp(j, n - 1, i) {
+            // 始発寄り到着時刻が早い場合
+            chmax(st, s[j]);
+            // 次の乗車時刻
+            if(st % f[j]) st = (ll)(st / f[j]) * (f[j] + 1);
+            // 電車に乗っている時間
+            st += c[j];
+        }
+        ans[i] = st;
+    }
+
+    rep(i, n) cout << ans[i] << endl;
+}
