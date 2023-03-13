c6a710446be8cb925e0c2b8328b5f9b2e4faff03
sisi0808 <52097096+sisi0808@users.noreply.github.com>
2023-03-13 13:52:21 +0900

    Update to delete dSYM folder when autocommit

--- delete_compiled_file.py
+++ delete_compiled_file.py
@@ -1,18 +1,27 @@
 import os
 import glob
 import re
+import shutil
 
 
-def remove_glob(pathname, regex, recursive=True):
+def remove_globed_file(pathname, regex, recursive=True):
     for p in glob.glob(pathname, recursive=recursive):
         if os.path.isfile(p) and re.search(regex, p):
             os.remove(p)
+
+
+def remove_globed_dir(pathname, regex, recursive=True):
+    for p in glob.glob(pathname, recursive=recursive):
+        if os.path.isdir(p) and re.search(regex, p):
+            shutil.rmtree(p)
 
 
 if __name__ == "__main__":
     # If you want to add target dir,
     # you should add contest_name
     contest_dir = ["abc", "arc", "typical90", "dp"]
-    regex = r"^[^\.]+$"
+    regex1 = r"^[^\.]+$"
+    regex2 = r"^.*dSYM$"
     for pathname in contest_dir:
-        remove_glob(pathname + "/**", regex)
+        remove_globed_file(pathname + "/**", regex1)
+        remove_globed_dir(pathname + "/**", regex2)
