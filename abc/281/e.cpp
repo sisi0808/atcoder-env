8724055507dd2eb30173d21e6b059168a1c0d9f0
sisi0808 <52097096+sisi0808@users.noreply.github.com>
2023-03-28 11:37:20 +0900

    abc281_e

--- abc/281/e.cpp
+++ abc/281/e.cpp
@@ -65,10 +65,68 @@ const string alp = "abcdefghijklmnopqrstuvwxyz";
 using mint = modint1000000007;
 // cout << fixed << setprecision(12);
 
+/*
+* i番目まで見た時に、大きい方からj番目だった数は、i+1を見た時に、大きい方からj+1,j,j-1番目にいる
+
+* 追加 or 削除される数が
+    * 番人v[k]より大きい場合: 影響なし
+    * 番人v[k]より小さい場合: 合計が変化
+    *
+    *
+* 番人と同じ数が何個あるかだけ管理する必要がある
+*/
+
 int main(void) {
     fio();
     ll n, m, k;
     cin >> n >> m >> k;
-    vector<ll> a(n);
+    vector<ll> a(n), b;
     rep(i, n) cin >> a[i];
+
+    // bに先頭k個をコピー
+    copy(a.begin(), a.begin() + m, back_inserter(b));
+    sort(ALL(b));
+
+    vector<ll> ans(n - m + 1);
+    multiset<ll> st_k; // 先頭k個
+    multiset<ll> st_l; // 先頭k個以降
+    rep(i, b.size()) {
+        if(i < k) {
+            st_k.insert(b[i]);
+            ans[0] += b[i];
+        } else st_l.insert(b[i]);
+    }
+    // m=k=1対策
+    st_k.insert(0);
+    st_l.insert(INF);
+
+    // iが消えて、i+mが増える
+    rep(i, n - m) {
+        ans[i + 1] = ans[i];
+        // 削除
+        // 先頭k個に含まれている時
+        if(st_k.find(a[i]) != st_k.end()) {
+            st_k.erase(st_k.find(a[i]));
+            st_k.insert(*st_l.begin());
+            st_l.erase(st_l.begin());
+            ans[i + 1] = ans[i + 1] - a[i] + *st_k.rbegin();
+        } else { // k個以降に含まれている時
+            st_l.erase(st_l.find(a[i]));
+        }
+
+        // 追加
+        // 先頭k個にはいる場合
+        if(a[i + m] < *st_k.rbegin()) {
+            // cout << *st_k.rbegin() << endl;
+            st_l.insert(*st_k.rbegin());
+            st_k.erase(prev(st_k.end()));
+            st_k.insert(a[i + m]);
+            ans[i + 1] = ans[i + 1] + a[i + m] - *st_l.begin();
+        } else { // k個以降に入る場合
+            st_l.insert(a[i + m]);
+        }
+    }
+
+    rep(i, n - m + 1) cout << ans[i] << " ";
+    cout << endl;
 }
