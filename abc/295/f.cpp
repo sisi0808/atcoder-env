3d0272c05986f64615bd7ddfdb11e78d8bd53182
sisi0808 <52097096+sisi0808@users.noreply.github.com>
2023-04-01 10:49:02 +0900

    abc053_d-f and abc059_d-f and abc167_e and abc281_f and abc295_e-f

--- abc/295/f.cpp
+++ abc/295/f.cpp
@@ -8,7 +8,7 @@ using namespace atcoder;
     ios::sync_with_stdio(false);
 using namespace std;
 #define rep(i, n) for(int i = 0; i < int(n); ++i)
-#define rrep(i, n) for(int i = n; i >= 0; --i)
+#define rrep(i, n, m) for(int i = n; i >= m; --i)
 #define repp(i, n, m) for(int i = m; i < int(n); ++i)
 #define fore(i_in, a) for(auto &i_in : a)
 #define ALL(v) (v).begin(), (v).end()
@@ -31,11 +31,11 @@ using graph = vector<vector<int>>;
 using Graph = vector<vector<ll>>;
 using P = pair<ll, ll>;
 const int SIZE = 100005;
-const int inf = 1 << 31;
+const int inf = 1 << 30;
 const int _inf = 1000000000;
 const int modi = 1000000007;
 const long long INF = 1LL << 62;
-const long long _INF = 1000000000000000000LL;
+const long long _INF = 1000000000000000LL;
 const long long modl = 1000000007LL;
 const long long modll = 998244353LL;
 
@@ -58,48 +58,13 @@ void YN(bool t) {
 
 vector<int> dx = {1, 0, -1, 0};
 vector<int> dy = {0, 1, 0, -1};
-const string ALP = "ABCDEFGHIkkKLMNOPQRSTUVWXYZ";
+const string ALP = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const string alp = "abcdefghijklmnopqrstuvwxyz";
 
-using mint = modint998244353;
-// using mint = modint1000000007;
+// using mint = modint998244353;
+using mint = modint1000000007;
 // cout << fixed << setprecision(12);
-
-/*
- * iブロックまで見た時の同じ色の組の数を管理
- * ではなく、あり得る数の管理？
- * 一番右の色も管理?
- * 色を区別しない以上、それもいらないか
- */
-
-// 組み合わせの数nCrを計算
-mint fact[200005];
-
-void init() {
-    fact[0] = 1;
-    rep(i, 200004) fact[i + 1] = fact[i] * (i + 1);
-}
-
-mint nCr(int n, int r) {
-    if(n == 0) return r == 0;
-    r = min(r, n - r);
-    if(r < 0) return 0;
-    auto nr = make_pair(n, r);
-    mint nu = fact[n];
-    mint de = fact[r] * fact[n - r];
-    return nu / de;
-}
 
 int main(void) {
     fio();
-    ll n, m, k;
-    cin >> n >> m >> k;
-
-    init();
-    mint ans = 0;
-    rep(i, k + 1) {
-        ans += nCr(n - 1, i) * m * pow_mod(m - 1, n - 1 - i, 998244353);
-    }
-
-    cout << ans.val() << endl;
 }
