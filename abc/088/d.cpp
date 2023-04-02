251516495c5ed92f4ebcfef17fc5a116c9e02a41
sisi0808 <52097096+sisi0808@users.noreply.github.com>
2023-04-03 07:26:32 +0900

    abc088_d

--- abc/088/d.cpp
+++ abc/088/d.cpp
@@ -65,6 +65,52 @@ const string alp = "abcdefghijklmnopqrstuvwxyz";
 using mint = modint1000000007;
 // cout << fixed << setprecision(12);
 
+/*
+白いマス目の数 - 最短経路の数
+*/
+ll h, w;
+vector<string> s;
+
+bool judge(int y, int x) {
+    if(y < 0 || x < 0) return false;
+    if(y >= h || x >= w) return false;
+    if(s[y][x] == '#') return false;
+    return true;
+}
+
 int main(void) {
     fio();
+    cin >> h >> w;
+    s.resize(h);
+    rep(i, h) cin >> s[i];
+
+    ll ans = 0;
+    rep(i, h) {
+        rep(j, w) {
+            if(s[i][j] == '.') ans++;
+        }
+    }
+
+    /* st = 始点 */
+    vector<vector<int>> dist(h, vector<int>(w, -1));
+    dist[0][0] = 0;
+    queue<P> pp;
+    pp.push({0, 0});
+
+    while(!pp.empty()) {
+        int y, x;
+        tie(y, x) = pp.front();
+        pp.pop();
+        rep(i, 4) {
+            int xx = x + dx[i];
+            int yy = y + dy[i];
+            if(!judge(yy, xx)) continue;
+            if(dist[yy][xx] == -1) {
+                dist[yy][xx] = dist[y][x] + 1;
+                pp.push({yy, xx});
+            }
+        }
+    }
+    if(dist[h - 1][w - 1] == -1) cout << -1 << endl;
+    else cout << ans - 1 - dist[h - 1][w - 1] << endl;
 }
